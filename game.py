0c5a670334d8e1aec11e237e48e060fd8e6c300e
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2012-03-06 21:50:59 -0800

    specifying host and port for heroku

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +29,8 @@ def end_game(game_id):
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port, debug = True)
+
+
