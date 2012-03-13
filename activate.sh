7a9cc4163130f6ab56fe7de623fd1f08e9e83cc5
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2012-03-12 18:32:56 -0700

    environment with tmux

--- activate.sh
+++
@@ -1,3 +0,0 @@
-# http://flask.pocoo.org/docs/installation/#installation
-#
-. venv/bin/activate
---
+++ env.sh
@@ -0,0 +1,26 @@
+S=number-guess
+W=awesome
+
+tmux has-session -t $S 
+if [ $? != 0 ] 
+then
+  tmux new-session -s $S -n $W -d
+  tmux send-keys 'vim test/*' C-m
+  
+  tmux split-window -h -t $W
+  tmux resize-pane -R -t $W 10
+  tmux send-keys 'source venv/bin/activate' C-m
+  tmux send-keys './test.sh' C-m
+  
+  tmux split-window -v -t $W
+  tmux send-keys 'source venv/bin/activate' C-m
+  tmux send-keys 'start server' C-m
+  
+  tmux split-window -v -t $W
+  tmux send-keys 'git status' C-m
+
+  tmux select-window -t $S:1
+fi 
+tmux attach -t $S
+
+
